5645b45ef371dfe9769c3dd233b6be6a01a6708a
Islam <islam.music@scv.si>
2025-03-07 11:19:36 +0100

    login

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,3 @@
-import Header from "../commponents/Header.tsx";
-import Footer from "../commponents/Footer.tsx";
 import Intro from "../commponents/Intro.tsx";
 import Album from "../commponents/Album.tsx";
 
